6659d20f2b68da52814f8fe7a0d55a3306e55d32
Mikhail Koryakin <sdrr87@gmail.com>
2023-12-18 18:16:07 +0300

    feat: remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,18 @@ const { ListNode } = require('../extensions/list-node.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while (l && l.value === k) {
+    l = l.next;
+  }
+
+  let current = l;
+
+  while (current && current.next) {
+    if (current.next.value === k) current.next = current.next.next;
+    else current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
